daa6e9abb152efbcf1f87660264299b6b92ef918
avalenz95 <audaris@gmail.com>
2024-05-09 17:14:48 -0400

    add create_install_dir function

--- scripts/minikube-install.py
+++ scripts/minikube-install.py
@@ -1,38 +1,68 @@
 #!/usr/bin/env python3
 
 import subprocess
-
-#check if minikube is installed
+import os
 
 INSTALL_DIR="/usr/local/bin"
 APP_NAME="minikube"
+OS_TYPE="linux"
+ARCH_TYPE="amd64"
 
+UNI_ERR = "\033[0;31m\u2717\033[0m"
+UNI_OUT = "\033[92m\u2714\033[0m"
+
+def download_minikube():
+    # TODO: look into subprocess/curl functionality. this fails to error when passed with invalid data
+    cmd = [f"curl -LO https://storage.googleapis.com/minikube/releases/latest/{APP_NAME}-{OS_TYPE}-{ARCH_TYPE}"]
+    sp = subprocess.run(cmd, shell=True)
+
+    if sp.returncode == 0:
+        print(f"{UNI_OUT} pulled {APP_NAME}-{OS_TYPE}-{ARCH_TYPE} binary")
+    else:
+        print(f"{UNI_ERR} error pulling binary: {sp.stderr}")
+        return sp.stderr
+
+def create_install_dir(directory):
+    try:
+        os.makedirs(directory)
+        print(f"{UNI_OUT} directory created at {directory}")
+        return True, None
+    
+    except FileExistsError as err:
+        print(f"directory already exists at {directory} procceeding...")
+        return True, err.strerror
+    
+    except OSError as err:
+        print(f"{UNI_ERR} failed creating directory at {directory} error: {err.strerror}")
+        return False, err.strerror
+    
 def install_minikube():
-    cmd = ["curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"]
-    res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-    #TODO:Configure subprocess to pipe directly to stdout/err?
+
     #TODO:Turn binary to executable, move to INSTALL_DIR, verify directory exists, create if not
     #TODO:configure install/curl to specify version/platform
-    print(res.stderr)
+    #print(sp.stderr)
+    pass
 
 def check_install():
     print(f"Checking {APP_NAME} installation...")
-    cmd = ['minikube version --short']
-    res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    cmd = ["minikube version --short"]
+    sp = subprocess.run(cmd, capture_output=True, text=True, shell=True)
     #TODO:clean output, verify return code
-    if res.returncode == 0:
-        app_version = res.stdout.strip()
-        print(f"\033[92m\u2713 {APP_NAME} verison: {app_version}")
-        return res.stdout.strip()
+    if sp.returncode == 0:
+        app_version = sp.stdout.strip()
+        print(f"{UNI_OUT}  {APP_NAME} verison: {app_version}")
+        return sp.stdout.strip()
     else:
-        err = res.stderr.strip()
-        print(f"\u274C {err}")
+        err = sp.stderr.strip()
+        print(f"{UNI_ERR}  {err}")
         return err
 
 
 
 if __name__ == "__main__":
-    install_minikube()
+    create_install_dir(f"{INSTALL_DIR}/{APP_NAME}")
+    #download_minikube()
+    #install_minikube()
     #check_install()
 
 #check if minikube is up to date/latest version
