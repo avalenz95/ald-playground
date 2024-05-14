6eaa1eb37627915779f79a7ce461bc0a7c7b3006
avalenz95 <audaris@gmail.com>
2024-05-14 13:31:37 -0400

    add init files

---
+++ scripts/tests/test_minikube_install.py
@@ -0,0 +1,12 @@
+import pytest
+
+from scripts import minikube_install
+
+def test_download_minikube() -> None:
+    pass
+
+def test_check_install() -> None:
+    pass
+
+def test_install_minikube() -> None:
+    pass
